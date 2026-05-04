bd0fdb62665c45bc8d379912211114a9686d189c
Chihaan <you@example.com>
2026-05-04 12:38:40 +0200

    add: file structured_ourput.py

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+language = "Python"
+version = 3
+pi = 3.141592653589793
+is_valid = 1 == 1
+
+print(f"Language: {language}")
+print(f"Version: {version}")
+print(f"Pi approx: {pi:.2f}")
+print(f"Computation valid: {is_valid}")
