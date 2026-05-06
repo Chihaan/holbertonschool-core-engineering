6fc9eecfe361c2399cbf74c562b5d28e9e2a0c74
Chihaan <you@example.com>
2026-05-06 11:55:07 +0200

    refactor :  calculation

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -3,9 +3,10 @@
 
 import calculator_1
 
+a = 10
+b = 5
+
 if __name__ == "__main__":
-    a = 10
-    b = 5
     print("{:d}".format(calculator_1.add(a, b)))
     print("{:d}".format(calculator_1.sub(a, b)))
     print("{:d}".format(calculator_1.mul(a, b)))
