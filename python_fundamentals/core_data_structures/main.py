295bdd692e95d7d0dc1172ef668683f6b7add551
Chihaan <you@example.com>
2026-05-06 13:32:15 +0200

    add: file 3 project 4

---
+++ python_fundamentals/core_data_structures/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+element_at = __import__('element_at').element_at
+replace_in_list = __import__('replace_in_list').replace_in_list
+
+my_list = ["a", "b", "c", "d", "e"]
+
+print()
+print("Ex:1")
+print()
+
+print(element_at(my_list, 3))
+print(element_at(my_list, -1))
+print(element_at(my_list, 15))
+print(element_at(my_list, 0))
+print(element_at(my_list, 1))
+print(element_at(my_list, -100))
+print(element_at(my_list, 13))
+print(element_at(my_list, 4))
+
+print()
+print("Ex:2")
+print()
+
+my_list = [1, 2, 3, 4, 5]
+print(replace_in_list(my_list, 3, 99))
+print(replace_in_list(my_list, 15, 99))
