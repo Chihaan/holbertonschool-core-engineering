66a830a9f0ef777fbf31139038f92358fe72d143
Chihaan <you@example.com>
2026-05-06 13:46:50 +0200

    refactor: file 4 project 4

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -2,7 +2,10 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for row in range(3):
-        for col in range(3):
-            print("{:d} ".format(matrix[row][col]), end="")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=" ")
+            else:
+                print("{:d}".format(matrix[i][j]), end="")
         print()
