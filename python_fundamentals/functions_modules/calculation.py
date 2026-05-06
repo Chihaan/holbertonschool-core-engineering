59f8083ee568a8ae518005d4286b089e6ad31cbe
Chihaan <you@example.com>
2026-05-06 12:01:37 +0200

    refactor :  calculation

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -9,7 +9,7 @@ from calculator_1 import div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{:d}".format(calculator_1.add(a, b)))
-    print("{:d}".format(calculator_1.sub(a, b)))
-    print("{:d}".format(calculator_1.mul(a, b)))
-    print("{:d}".format(calculator_1.div(a, b)))
+    print("{:d}".format(add(a, b)))
+    print("{:d}".format(sub(a, b)))
+    print("{:d}".format(mul(a, b)))
+    print("{:d}".format(div(a, b)))
