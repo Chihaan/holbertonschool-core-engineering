3419ed58db0b8f63fed3f5105b82b6dc9ae15366
Chihaan <you@example.com>
2026-05-06 11:19:58 +0200

    refactor: pow

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -5,7 +5,8 @@ def pow(a, b):
     result = 1
     if b < 0:
         for i in range(-b):
-            result /= a
+            result *= a
+        return 1 / result
     else:
         for i in range(b):
             result *= a
