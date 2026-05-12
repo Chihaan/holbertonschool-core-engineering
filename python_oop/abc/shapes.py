b0e2643ad60fb118003831a4401f4ab42372ba1a
Chihaan <gavet.vadim@gmail.com>
2026-05-12 22:47:46 +0200

    add: all files of project 3

---
+++ python_oop/abc/shapes.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+
+from math import pi
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius * self.radius
+    
+    def perimeter(self):
+        return 2 * pi * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return self.width * 2 + self.height * 2
+    
+def shape_info(shape):
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
