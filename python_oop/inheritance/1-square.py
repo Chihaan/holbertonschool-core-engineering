1d78e45170ea29357208e06111476cd256eab7b2
Chihaan <gavet.vadim@gmail.com>
2026-05-13 16:58:32 +0200

    refactor: verification logic

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -9,3 +9,5 @@ class Square(Rectangle):
     def __init__(self, size):
         """Init avec size."""
         super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
