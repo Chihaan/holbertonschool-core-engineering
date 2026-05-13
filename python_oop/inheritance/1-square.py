a6a94ed3b6e0ab522fb180713a6a9102c256bb99
Chihaan <gavet.vadim@gmail.com>
2026-05-13 16:50:49 +0200

    refactor: verification logic

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Square hérite de Rectangle."""
 
-BaseGeometry = __import__('base_geometry').BaseGeometry
+Rectangle = __import__('2-rectangle').Rectangle
 
 
 class Square(Rectangle):
