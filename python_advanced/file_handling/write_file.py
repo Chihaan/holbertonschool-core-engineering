fbde6de3258e1e5f0c11cb48a31683ed24f7cbc5
Chihaan <gavet.vadim@gmail.com>
2026-05-27 09:40:56 +0200

    change: name of folder

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -2,6 +2,6 @@
 
 
 def write_file(filename="", text=""):
+    """Write a string to a text file and return the number of characters written."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
-    
