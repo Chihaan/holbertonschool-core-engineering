18030622c24cb33327d2654ab0939fdce1fbcf5b
Chihaan <gavet.vadim@gmail.com>
2026-05-27 09:43:05 +0200

    change: name of folder

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -2,6 +2,6 @@
 
 
 def append_write(filename="", text=""):
-    """Append a string to a text file and return the number of characters added."""
+    """Append a string to a text file and return number characters added."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -2,6 +2,6 @@
 
 
 def write_file(filename="", text=""):
-    """Write a string to a text file and return the number of characters written."""
+    """Write a string to a text file and return number characters written."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
