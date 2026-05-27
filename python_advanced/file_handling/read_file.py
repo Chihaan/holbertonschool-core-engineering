a4e8f788a1bd0a02d5c7740a3415075a133e33fe
Chihaan <gavet.vadim@gmail.com>
2026-05-27 09:48:59 +0200

    change: name of folder

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -4,4 +4,4 @@
 def read_file(filename=""):
     """Read a text file and print its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
