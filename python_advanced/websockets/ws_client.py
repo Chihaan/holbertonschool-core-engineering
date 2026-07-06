fc9dfdfef70cf842431bfbeecdc72691b8fb202c
Chihaan <gavet.vadim@gmail.com>
2026-07-06 10:54:18 +0200

    modify: file 1 websockets

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -14,7 +14,7 @@ async def connect_and_send(uri, message):
 if __name__ == "__main__":
     uri = os.environ.get("WS_URI", "ws://localhost:8765")
     if len(sys.argv) > 1:
-        message = sys.argv[1]
+        message = sys.argv[2]
     else:
         message = os.environ.get("WS_MSG", "Hello WebSocket")
     asyncio.run(connect_and_send(uri, message))
