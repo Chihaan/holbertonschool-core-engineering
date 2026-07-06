8948c9aeffab2173cee230dbc0bb2d2e28b85f13
Chihaan <gavet.vadim@gmail.com>
2026-07-06 12:08:43 +0200

    add: file 5 websockets

--- python_advanced/websockets/asgi_server.py
+++ python_advanced/websockets/asgi_server.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
+
 from starlette.applications import Starlette
 from starlette.responses import HTMLResponse
 from starlette.routing import Route, WebSocketRoute
 from starlette.websockets import WebSocketDisconnect
 
+
 async def homepage(request):
     return HTMLResponse("<h1>WebSocket App</h1>")
+
 
 async def websocket_endpoint(websocket):
     await websocket.accept()
