1fe1103012afc38391af744947d90942c3702098
Chihaan <gavet.vadim@gmail.com>
2026-07-06 10:29:32 +0200

    modify: file 1 websockets

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -4,7 +4,7 @@ import asyncio
 import websockets
 
 
-async def main():
+async def connect_and_send():
     async with websockets.connect("ws://localhost:8765") as ws:
         await ws.send("Hello WebSocket")
         reponse = await ws.recv()
@@ -12,4 +12,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(connect_and_send())
