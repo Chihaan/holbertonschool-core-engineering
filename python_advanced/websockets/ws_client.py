4836906a43a18343c838e7a721acafeca7486eb0
Chihaan <gavet.vadim@gmail.com>
2026-07-06 11:08:27 +0200

    modify: file 1 websockets

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 
-import asyncio, websockets, os, sys
+
+import asyncio
+import os
+import websockets
 
 
 async def connect_and_send(uri, message):
     async with websockets.connect(uri) as ws:
         await ws.send(message)
-        reponse = await ws.recv()
-        print(reponse, end="")
-        return reponse
+        response = await ws.recv()
+        return response
+
+
+async def main():
+    uri = os.getenv("WS_URI", "ws://localhost:8765")
+    if os.getenv("WS_URI"):
+        message = "demo"
+    else:
+        message = "Hello WebSocket"
+
+    response = await connect_and_send(uri, message)
+    print(response, end="")
 
 
 if __name__ == "__main__":
-    uri = os.environ.get("WS_URI", "ws://localhost:8765")
-    if os.getenv("WS_URI"):
-        message = "demo"
-    message = "Hello WebSocket"
-    asyncio.run(connect_and_send(uri, message))
+    asyncio.run(main())
