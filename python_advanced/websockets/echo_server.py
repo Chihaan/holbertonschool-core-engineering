988e40fb76298fc5b7726b2091fc872bbe289f97
Chihaan <gavet.vadim@gmail.com>
2026-07-06 10:15:42 +0200

    add file 0 websockets

---
+++ python_advanced/websockets/echo_server.py
@@ -0,0 +1,14 @@
+import asyncio
+import websockets
+
+
+async def handler(websocket):
+    async for message in websocket:
+        print("reçu:", message)
+        await websocket.send(message)
+
+async def main():
+    async with websockets.serve(handler, "localhost", 8765):
+        await asyncio.Future()
+
+asyncio.run(main())
