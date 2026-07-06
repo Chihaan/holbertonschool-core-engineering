0f5c30857f6a826c8bf3a0eaf0f629b5e7a9c143
Chihaan <gavet.vadim@gmail.com>
2026-07-06 10:35:34 +0200

    modify: file 1 websockets

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -4,12 +4,13 @@ import asyncio
 import websockets
 
 
-async def connect_and_send():
-    async with websockets.connect("ws://localhost:8765") as ws:
-        await ws.send("Hello WebSocket")
+async def connect_and_send(uri, message):
+    async with websockets.connect(uri) as ws:
+        await ws.send(message)
         reponse = await ws.recv()
         print(reponse)
+        return reponse
 
 
 if __name__ == "__main__":
-    asyncio.run(connect_and_send())
+    asyncio.run(connect_and_send(("ws://localhost:8765", "Hello WebSocket")))
