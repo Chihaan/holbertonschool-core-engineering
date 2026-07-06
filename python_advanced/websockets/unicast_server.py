ec27cd773c064c28e6668df7c6a73bae81ad3c07
Chihaan <gavet.vadim@gmail.com>
2026-07-06 11:40:29 +0200

    add: file 3 websockets

---
+++ python_advanced/websockets/unicast_server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import asyncio
+import websockets
+
+
+connected = set()
+
+
+async def connection_handler(websocket):
+    try:
+        async for message in websocket:
+            await websocket.send("U:", message)
+    finally:
+        connected.remove(websocket)
+
+async def main():
+    async with websockets.serv(connection_handler, "localhost", 8765):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
